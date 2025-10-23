a2d4b26808c186572a1f0b12307439b56476b2ca
tennydibooglu <a.dibooglu@wustl.edu>
2025-10-23 11:52:55 -0500

    Finished

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -3,7 +3,14 @@ package studio6;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class RecursiveMethods {
-
+	public static void main(String[] args) {
+		System.out.println(geometricSum(5));
+		int[] testArray = {1,2,3,4,5,6};
+		for(int x: toReversed(testArray)){
+		System.out.println(x); 
+		} 
+		System.out.println(gcd(18, 24));
+	}
 	/**
 	 * Computes the geometric sum for the first n terms in the series
 	 * 
@@ -11,13 +18,15 @@ public class RecursiveMethods {
 	 * @return the sum of the first n terms of the geometric series (1/2 + 1/4 + 1/8
 	 *         ...)
 	 */
+	static double sum = 0;
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
-		
+		if(n>0){
+			sum += (1.0/Math.pow(2, n));
+			return geometricSum(n-1);
+		} else {
+			return sum;
 	}
-
+	}
 	
 	/**
 	 * @param xCenter                       x-coordinate of the center of the circle
@@ -29,18 +38,15 @@ public class RecursiveMethods {
 	 * @param radiusMinimumDrawingThreshold radius above which drawing should occur
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius, double radiusMinimumDrawingThreshold) {
+		double scalar = 1.0/3.0;
 		StdDraw.circle(xCenter, yCenter, radius);
-		if (radiusMinimumDrawingThreshold < radius){
-			circlesUponCircles(xCenter - radius*(1/3), yCenter, radius * 1/3, radiusMinimumDrawingThreshold);
-			circlesUponCircles(xCenter + radius*(1/3), yCenter, radius * 1/3, radiusMinimumDrawingThreshold);
-			circlesUponCircles(xCenter, yCenter - radius*(1/3), radius * 1/3, radiusMinimumDrawingThreshold);
-			circlesUponCircles(xCenter, yCenter + radius*(1/3), radius * 1/3, radiusMinimumDrawingThreshold);
+		if (radius - radius*scalar > radiusMinimumDrawingThreshold){
+			circlesUponCircles(xCenter + radius, yCenter, radius*scalar, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter - radius, yCenter, radius*scalar, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter, yCenter + radius, radius*scalar, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter, yCenter - radius, radius*scalar, radiusMinimumDrawingThreshold);
 		}
 		// FIXME complete the recursive drawing
-	}
-	
-	public static void main(String[] args){
-		circlesUponCircles(0.5,0.5,.4,.1);
 	}
 
 	/**
@@ -49,11 +55,32 @@ public class RecursiveMethods {
 	 * @param array the array to create a reverse of, not to be mutated
 	 * @return an array with the same data as the input but it reverse order
 	 */
+	/*public static int[] getReversedArray(int[] arr, int cnt){
+		if(cnt < arr.length/2) {
+			int temp = arr[cnt];
+			arr[cnt] = arr[arr.length - cnt - 1];
+			arr[arr.length - cnt - 1] = temp;
+			return getReversedArray(arr, cnt+1);
+		} else {
+			return arr;
+		}
+	}
+	*/
+	public static int[] getReversedArray(int[] arr1, int[] arr2, int cnt){
+		if(cnt < arr1.length){
+			arr1[cnt] = arr2[arr2.length - cnt - 1];
+			return getReversedArray(arr1, arr2, cnt+1);
+		} else {
+			return arr1;
+		}
+	}
 	public static int[] toReversed(int[] array) {
-		
+		// note to friends, make sure you intialize a new array instead of setting int[] newArray = array; I spent three hours mulling over my logic
+		int[] modifiedArray = new int[array.length];
+		int cnt = 0;
+		getReversedArray(modifiedArray, array, cnt);
 			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
-		
+			return modifiedArray;
 	}
 
 	/**
@@ -65,9 +92,16 @@ public class RecursiveMethods {
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		
+			//copied the while loop, while loops and recursion are very similar
+			if (q != 0){ 
+				int temp = q;
+				q = p % q;
+				p = temp;
+				return gcd(p, q);
+			} else {
+				return p;
+			}
 			// FIXME compute the gcd of p and q using recursion
-			return 0;
 		
 	}
 
